f40e53cca00fbf45812c9f1d6ba8f4a610a64bde
赵智博 <53853830+zhao-zhibo@users.noreply.github.com>
2024-07-17 22:29:04 +0800

    Update task.cpp

--- src/task.cpp
+++ src/task.cpp
@@ -15,11 +15,8 @@ int main(int argc, char const *argv[])
     //@ homePage: https://space.bilibili.com/336103007
     //@ github: https://github.com/cckaixin/Practical_Homework_for_slambook14
     /*********************************************************************************************************/
-    // 下面的过程是将1系固定，然后固定轴旋转，先绕Z转90度，再绕Y转-90度，最后得到的矩阵是R12,也就是R_1_2
-    // R_1_2是从1系旋转到2系得到的旋转矩阵，但是确是将2系下的点转到1系下的点的旋转矩阵。
-    // 比如 我们所说的P1 = R_1_2 * P2 + t_1_2中，R_1_2和 t_1_2确实是从右向左读没错，但这只是坐标变换时的较为合适的读法
-    // R_1_2实际对应的是坐标系1到坐标系2的旋转，我们之所以说R_1_2角标从右往左读，那是对于坐标变化来说的，对坐标轴的姿态变换要从左往右读才是真实旋转方向，这点很重要！
-    // 同理，t_1_2实际对应的是坐标系1原点指向坐标系2原点的向量，我们之所以说t12角标从右往左读，那是对于坐标变换来说的，对坐标轴的位置变换要从左往右读才是真实平移向量方向，这点很重要！
+    // 将1系固定，然后固定轴旋转，先绕Z转90度，再绕Y转-90度，最后得到的矩阵是R12,也就是R_1_2
+    // t_1_2是1坐标系的原点在2坐标系下的坐标值
 
     // 第一步：在1坐标系下围绕Z轴旋转90度  yaw是航向角-Z轴
     const double angle_z = M_PI_2; // 90度转换为弧度
@@ -39,11 +36,17 @@ int main(int argc, char const *argv[])
     //            1  6.12323e-17            0
     //  6.12323e-17           -1  6.12323e-17
 
-    // 测试点
-    Vector3d point(1, 1, 1);
-    Vector3d point_transformed = final_rotation.transpose() * point;
-    std::cout << "Point in Frame2: " << point_transformed.transpose() << std::endl;
+    Vector3d t_1_2(0, 0, 0); // 1系原点在2系下的坐标
 
+    // 测试点 1
+    Vector3d pointA(1, 1, 1); // 1坐标系下的点
+    Vector3d pointA_transformed = final_rotation.transpose() * pointA + t_1_2;
+    std::cout << "Point A in Frame2: " << pointA_transformed.transpose() << std::endl;
+
+    // 测试点 2
+    Vector3d pointB(0, 2, 0); // 1坐标系下的点
+    Vector3d pointB_transformed = final_rotation.transpose() * pointB + t_1_2;
+    std::cout << "Point B in Frame2: " << pointB_transformed.transpose() << std::endl;
 
 
     // -----------------------------------------------------
@@ -51,9 +54,8 @@ int main(int argc, char const *argv[])
     // -----------------------------------------------------
 
     // LIO-SAM中的extrinsicRot的计算过程
-    // 将雷达坐标系旋转到imu坐标系，也就是R_lidar_imu
-    // 具体的旋转是固定雷达坐标系，将雷达坐标系旋转到imu坐标系，绕雷达系的y轴旋转180度，得到和imu坐标系一样的坐标系，也就是R_lidar_imu
-    // 但是点的对应关系是 P_lidar = R_lidar_imu * P_imu
+    // 将imu坐标系旋转到雷达坐标系
+    // 具体的旋转是固定imu坐标系，将imu坐标系旋转到雷达坐标系，绕imu系的y轴旋转180度，得到和雷达坐标系一样的坐标系
     const double angle1 = M_PI; 
     AngleAxisd angle1_lio_y(angle1, Vector3d::UnitY());
     Matrix3d extrinsicRot = angle1_lio_y.toRotationMatrix(); 
@@ -65,9 +67,9 @@ int main(int argc, char const *argv[])
 
     // -----------------------------------------------------
 
-    // LIO-SAM中的extQRPY的计算过程，也就是R_quat_lidar
-    // 将quat坐标系旋转到lidar坐标系，也就是固定quat，旋转quat到lidar坐标系
-    // 固定quat坐标系，绕quar坐标系的z轴旋转-90度，得到和lidar坐标系一样的坐标系
+    // LIO-SAM中的extQRPY的计算过程，也就是R_mag_lidar
+    // 将磁力计坐标系旋转到lidar坐标系
+    // 固定磁力计坐标系，绕磁力计坐标系的z轴旋转-90度，得到和lidar坐标系一样的坐标系
     const double angel2 = -M_PI_2; 
     AngleAxisd angle2_lio_z(angel2, Vector3d::UnitZ());
     Matrix3d extQRPY = angle2_lio_z.toRotationMatrix(); 
